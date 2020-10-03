6454db0f09c997ebd7627f5aeaa271fc8c3a3a7b
RishabBaisakh <rishab.baisakh@gmail.com>
2020-10-03 19:33:24 -0400

    Added sidebar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
       <h1>Let's start cloning slack</h1>
 
-      {/* Header */}
       <Header />
-      {/* Sidebar */}
+
+      <div className="app_body">
+        {/* Sidebar */}
+        <Sidebar />
+      </div>
       {/* React router -> Chat screen */}
     </div>
   );
