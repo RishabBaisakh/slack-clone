ae9df77744c1e8b833a0459f1d88568aad8c38bb
RishabBaisakh <rishab.baisakh@gmail.com>
2020-10-19 02:52:21 -0400

    added welcome component

---
+++ src/Welcome.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Welcome.css";
+
+function Welcome() {
+  return (
+    <div className="welcome">
+      <div className="welcome_header">
+        <h3>Welcome, start sharing your ideas.</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Welcome;
